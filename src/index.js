2b1e1d8f288e9b506993f9aeb74f7901958c5a71
Suyog <89405908+ErSuyog16@users.noreply.github.com>
2022-06-16 09:49:35 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>hiii this is suyog</h1>
+    <p>this is paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
